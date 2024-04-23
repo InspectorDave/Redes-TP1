51dd5a7ed87bae4e17de960e9ae387b6beb1aea3
FabioSicca <fsicca@fi.uba.ar>
2024-04-23 16:46:01 -0300

    Agrego los parsers de 'upload.py' y 'download.py'

---
+++ src/download.py
@@ -0,0 +1,4 @@
+from lib.args_parser import parse_download_arguments
+
+if __name__ == "__main__":
+    args = parse_download_arguments()
--- src/lib/args_parser.py
+++ src/lib/args_parser.py
@@ -3,8 +3,10 @@ import argparse
 def parse_server_arguments():
     parser = argparse.ArgumentParser(prog='start-server.py', description='<command description>')
 
-    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-    parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
+    group_verbosity = parser.add_mutually_exclusive_group(required=False)
+    group_verbosity.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    group_verbosity.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
+    
     parser.add_argument('-H', '--host', action='store', type=str, help='service IP address')
     parser.add_argument('-p', '--port', action='store', type=int, help='service port')
     parser.add_argument('-s', '--storage', action='store', type=str, help='storage dir path')
@@ -12,19 +14,40 @@ def parse_server_arguments():
     args = parser.parse_args()
     
     if args.verbose:
-        print("Verbose mode enabled")
-    elif args.quiet:
-        print("Quiet mode enabled")
-    if args.host:
-        print("Host:", args.host)    
-    if args.port:
-        print("Port:", args.port)
-    if args.storage:
-        print("Storage directory:", args.storage)
+        print(args)
+
     return args
 
 def parse_upload_arguments():
-    return
+    parser = argparse.ArgumentParser(prog='upload.py', description='<command description>')
+    _add_common_args(parser)
+    parser.add_argument('-s', '--src', action='store', type=str, help='source file path')
+    parser.add_argument('-n', '--name', action='store', type=str, help='file name')
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(args)
+    return args
 
 def parse_download_arguments():
+    parser = argparse.ArgumentParser(prog='download.py', description='<command description>')
+    _add_common_args(parser)
+
+    parser.add_argument('-d', '--dst', action='store', type=str, help='destination file path')
+    parser.add_argument('-n', '--name', action='store', type=str, help='file name')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(args)
+    return args
+
+
+def _add_common_args(parser):
+    group_verbosity = parser.add_mutually_exclusive_group(required=False)
+    group_verbosity.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    group_verbosity.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
+    
+    parser.add_argument('-H', '--host', action='store', type=str, help='service IP address')
+    parser.add_argument('-p', '--port', action='store', type=int, help='service port')
     return
