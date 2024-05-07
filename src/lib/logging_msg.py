e43163ef6f7eb139120fe1504dfbe758b5cc1296
Ezequiel Mundani Vegega <emundani@fi.uba.ar>
2024-05-07 16:40:16 -0300

    refactor: removed linespaces in logging_msg.py

--- src/lib/logging_msg.py
+++ src/lib/logging_msg.py
@@ -1,63 +1,31 @@
 MSG_SERVER_READY_TO_RECEIVE = "The server is ready to receive"
-
 MSG_PROCESSING_NEW_CONNECTION = "Processing new connection..."
-
 MSG_IS_NOT_INITIATE = "Message isn't INITIATE"
-
 MSG_CLIENT_ALREADY_HAS_ASSIGNED_CONNECTION = "Client already has an assigned connection"
-
 MSG_RECEIVED_INITIATE = "Received INITIATE"
-
 MSG_PROCESSED_NEW_CONNECTION = "Processed new connection."
-
 MSG_PROCESSING_EXISTING_CONNECTION = "Processing an existing connection..."
-
 MSG_RECEIVED_MSG_TYPE = "Received message type:"
-
 MSG_IS_NOT_SENACK = "Message isn't SENACK"
-
 MSG_SENDING_INACK = "Sending inack..."
-
 MSG_SENDING_INITIATE = "Sending INITIATE"
-
 MSG_SENT_INACK = "Sent inack."
-
 MSG_IS_NOT_INACK = "Message isn't Inack"
-
 MSG_RECEIVED_INACK = "Received an Inack"
-
 MSG_SENDING_INACK = "Sending SENACK"
-
 MSG_BYTES_SENT = "Bytes sent: "
-
 MSG_HANDSHAKE_STARTING = "Handshake starting..."
-
 MSG_HANDSHAKE_COMPLETED = "Handshake completed"
-
 MSG_SEND_FILE_METHOD_NOT_IMPLEMENTED = "'send_file' method must be implemented in subclass"
-
 MSG_RECEIVE_FILE_METHOD_NOT_IMPLEMENTED = "'receive_file' method must be implemented in subclass"
-
 MSG_SENT_USING_GO_BACK_N = "Sending file using Go-Back-N Protocol"
-
 MSG_RECEIVED_USING_GO_BACK_N = "Receiving file using Go-Back-N Protocol"
-
 MSG_SENDING_FILE_USING_STOP_AND_WAIT = "Sending file using Stop and Wait Protocol"
-
 MSG_SENT_TYPE = "Sent message type: "
-
 MSG_WITH_SEQUENCE_N = ", with sequence: "
-
 MSG_NO_ACK_RECEIVED = "No ACK received"
-
 MSG_WITH_ACK_N = ", with ACK: "
-
 MSG_STORAGE_PATH = "storage_path: "
-
 MSG_BYTES_RECEIVED = "Bytes received: "
-
-MSG_WRITING_FILE_PATH = "[LOG] Writing file in "
-
+MSG_WRITING_FILE_PATH = "Writing file in "
 MSG_BYTES_WRITTEN = "Bytes written: "
-
-
