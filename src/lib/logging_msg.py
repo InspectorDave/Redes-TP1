24d02685ee558ab7f6773c7eb6da1569a612054d
FabioSicca <fsicca@fi.uba.ar>
2024-05-07 13:47:22 -0300

    refactor: quito [LOG] de los mensajes de debug

--- src/lib/logging_msg.py
+++ src/lib/logging_msg.py
@@ -4,39 +4,39 @@ MSG_PROCESSING_NEW_CONNECTION = "Processing new connection..."
 
 MSG_IS_NOT_INITIATE = "Message isn't INITIATE"
 
-MSG_CLIENT_ALREADY_HAS_ASSIGNED_CONNECTION = "[LOG] Client already has an assigned connection"
+MSG_CLIENT_ALREADY_HAS_ASSIGNED_CONNECTION = "Client already has an assigned connection"
 
-MSG_RECEIVED_INITIATE = "[LOG] Received INITIATE"
+MSG_RECEIVED_INITIATE = "Received INITIATE"
 
-MSG_PROCESSED_NEW_CONNECTION = "[LOG] Processed new connection."
+MSG_PROCESSED_NEW_CONNECTION = "Processed new connection."
 
-MSG_PROCESSING_EXISTING_CONNECTION = "[LOG] Processing an existing connection..."
+MSG_PROCESSING_EXISTING_CONNECTION = "Processing an existing connection..."
 
-MSG_RECEIVED_MSG_TYPE = "[LOG] Received message type:"
+MSG_RECEIVED_MSG_TYPE = "Received message type:"
 
-MSG_IS_NOT_SENACK = "[LOG] Message isn't SENACK"
+MSG_IS_NOT_SENACK = "Message isn't SENACK"
 
-MSG_SENDING_INACK = "[LOG] Sending inack..."
+MSG_SENDING_INACK = "Sending inack..."
 
-MSG_SENDING_INITIATE = "[LOG] Sending INITIATE"
+MSG_SENDING_INITIATE = "Sending INITIATE"
 
-MSG_SENT_INACK = "[LOG] Sent inack."
+MSG_SENT_INACK = "Sent inack."
 
-MSG_IS_NOT_INACK = "[LOG] Message isn't Inack"
+MSG_IS_NOT_INACK = "Message isn't Inack"
 
-MSG_RECEIVED_INACK = "[LOG] Received an Inack"
+MSG_RECEIVED_INACK = "Received an Inack"
 
-MSG_SENDING_INACK = "[LOG] Sending SENACK"
+MSG_SENDING_INACK = "Sending SENACK"
 
 MSG_BYTES_SENT = "Bytes sent: "
 
-MSG_HANDSHAKE_STARTING = "[LOG] Handshake starting..."
+MSG_HANDSHAKE_STARTING = "Handshake starting..."
 
-MSG_HANDSHAKE_COMPLETED = "[LOG] Handshake completed"
+MSG_HANDSHAKE_COMPLETED = "Handshake completed"
 
-MSG_SEND_FILE_METHOD_NOT_IMPLEMENTED = "El metodo 'send_file' debe ser implementado por la subclase"
+MSG_SEND_FILE_METHOD_NOT_IMPLEMENTED = "'send_file' method must be implemented in subclass"
 
-MSG_RECEIVE_FILE_METHOD_NOT_IMPLEMENTED = "El metodo 'receive_file' debe ser implementado por la subclase"
+MSG_RECEIVE_FILE_METHOD_NOT_IMPLEMENTED = "'receive_file' method must be implemented in subclass"
 
 MSG_SENT_USING_GO_BACK_N = "Sending file using Go-Back-N Protocol"
 
@@ -44,11 +44,11 @@ MSG_RECEIVED_USING_GO_BACK_N = "Receiving file using Go-Back-N Protocol"
 
 MSG_SENDING_FILE_USING_STOP_AND_WAIT = "Sending file using Stop and Wait Protocol"
 
-MSG_SENT_TYPE = "[LOG] Sent message type: "
+MSG_SENT_TYPE = "Sent message type: "
 
 MSG_WITH_SEQUENCE_N = ", with sequence: "
 
-MSG_NO_ACK_RECEIVED = "[LOG] No ACK received"
+MSG_NO_ACK_RECEIVED = "No ACK received"
 
 MSG_WITH_ACK_N = ", with ACK: "
 
